ca067353e8055ec37992c9be86cff53c1b226384
mrAJAY1 <thisismeajayprakash@gmail.com>
2023-05-24 04:08:49 +0530

    initial server setup

---
+++ app.js
@@ -0,0 +1,32 @@
+const app = require("express")();
+const { json } = require("express");
+const cors = require("cors");
+const cookieParser = require("cookie-parser");
+const mongoose = require("mongoose");
+const userRouter = require('./routes/userRouter.js')
+
+// configure dotenv
+require("dotenv").config();
+const { PORT, MONGO_URI } = process.env;
+
+// middleware configs
+app.use(json());
+app.use(cookieParser());
+app.use(cors({ origin: "*" }));
+
+// route configs
+app.get("/",(req,res) => {
+    res.send("server is active.")
+})
+app.use("/api/user",userRouter);
+
+
+// db configs
+mongoose.connect(MONGO_URI);
+const db = mongoose.connection;
+
+db.once("open", () => console.log(`DB connected successfully to ${db.name}`));
+db.on("error", console.error.bind(console, "db error : "));
+
+// setup server
+app.listen(PORT, () => console.log(`server listening on port ${PORT}`));
