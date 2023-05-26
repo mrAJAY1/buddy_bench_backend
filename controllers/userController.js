fb916ecb24d25c8ece7d9cb807561c299ff576ef
mrAJAY1 <thisismeajayprakash@gmail.com>
2023-05-26 06:18:32 +0530

    Refactor user module exports and destructuring in main code file

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,1 +1,9 @@
-const User = require("../models/user.js");
+const { login, signup, logout, updateUser, deleteUser } = require("../models/user.js");
+
+module.exports = {
+    login: ({ body }, res) => {},
+    signup: ({ body }, res) => {},
+    logout: (req, res) => {},
+    updateUser: ({ params, body }, res) => {},
+    deleteUser: ({ params }, res) => {},
+};
