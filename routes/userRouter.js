8ef3a6e742788907c83c3b7bcdef8e42f11c176f
mrAJAY1 <thisismeajayprakash@gmail.com>
2023-05-26 06:08:30 +0530

    added route paths without defining callbacks

--- routes/userRouter.js
+++ routes/userRouter.js
@@ -4,5 +4,10 @@ const controller = require("../controllers/userController.js");
 router.get("/", (req, res) => {
   res.send("server is active");
 });
+router.post("/login",(req,res)=>{});
+router.post("/signup",(req,res)=>{});
+router.post("/logout",(req,res)=>{})
+router.put("/update-user/:id",(req,res)=>{});
+router.delete("/delete-user/:id",(req,res)=>{});
 
 module.exports = router;
