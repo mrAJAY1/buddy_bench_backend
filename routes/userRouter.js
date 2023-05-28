bc848cf416a535d954dad68087fd290eb2f2349a
mrAJAY1 <thisismeajayprakash@gmail.com>
2023-05-28 09:43:23 +0530

    Add JWT authentication functions for auth middleware. updated logout and clear cookie functions

--- routes/userRouter.js
+++ routes/userRouter.js
@@ -1,14 +1,15 @@
 const router = require('express').Router();
 const controller = require('../controllers/userController.js');
+const authMiddleware = require('../middlewares/authMiddleware.js');
 
 // Routes for authentication
 router.post('/login', controller.login);
 router.post('/signup', controller.signup);
-router.post('/logout', controller.logout);
+router.post('/logout', authMiddleware, controller.logout);
 
 // Routes for user management
-router.put('/update-user/:id', controller.updateUser);
-router.delete('/delete-user/:id', controller.deleteUser);
+router.put('/update-user/:id', authMiddleware, controller.updateUser);
+router.delete('/delete-user/:id', authMiddleware, controller.deleteUser);
 
 // Default route
 router.get('/', (req, res) => {
